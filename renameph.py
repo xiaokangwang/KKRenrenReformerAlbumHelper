74f97aec293ba5fe622798baf24ea961b5e96629
Xiaokang Wang <xiaokangwang@outlook.com>
2013-05-25 18:57:04 +0800

    source file read added

--- renameph.py
+++ renameph.py
@@ -2,6 +2,9 @@
 import re
 import argparse
 
+def reimageline(stringtoparse):
+	pass
+
 #show brief use of this script
 
 print("KKRenrenReformer --- a script help user to parse saved album by Renren Reformer")
@@ -12,7 +15,7 @@ thearg = argparse.ArgumentParser()
 group = thearg.add_mutually_exclusive_group()
 group.add_argument("-v", "--verbose", action="store_true")
 group.add_argument("-q", "--quiet", action="store_true")
-thearg.add_argument("file", type=list, help="the file to be proceed")
+thearg.add_argument("file", help="the file to be proceed")
 args = thearg.parse_args()
 
 #make args into data to be used later
@@ -21,5 +24,9 @@ filename=args.file
 
 #Open file
 
-#f = open(filename, 'r')
+f = open(filename, 'r')
+htmldata=f.read()
+lines=reimageline(htmldata)
+
+
 
